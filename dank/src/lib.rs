27ae658482d74da1f037598df447751a3f5abdbf
Nima Rasooli <nimarasooli1@gmail.com>
2021-07-15 18:28:41 +0430

    Added name method.

--- dank/src/lib.rs
+++ dank/src/lib.rs
@@ -7,6 +7,11 @@ use ic_cdk::caller;
 use ic_cdk::export::Principal;
 use ic_cdk_macros::*;
 
+#[query]
+fn name() -> String {
+    String::from("Dank")
+}
+
 #[update]
 fn whoami() -> Principal {
     caller()
