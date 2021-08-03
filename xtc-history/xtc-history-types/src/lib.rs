3b64656808590e6000bca92547f47f3eb0a6ab66
qti3e <parsa@fleek.co>
2021-08-03 18:44:35 +0430

    move history types to xtc-history-types pkg

--- xtc-history/xtc-history-types/src/lib.rs
+++ xtc-history/xtc-history-types/src/lib.rs
@@ -1,7 +1,50 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+use ic_cdk::export::candid::CandidType;
+use ic_cdk::export::Principal;
+use serde::Deserialize;
+
+#[derive(CandidType, Clone, Deserialize)]
+pub enum TransactionKind {
+    Transfer {
+        from: Principal,
+        to: Principal,
+    },
+    Mint {
+        to: Principal,
+    },
+    Burn {
+        from: Principal,
+        to: Principal,
+    },
+    CanisterCalled {
+        canister: Principal,
+        method_name: String,
+    },
+    CanisterCreated {
+        canister: Principal,
+    },
+    ChargingStationDeployed {
+        canister: Principal,
+    },
 }
+
+#[derive(CandidType, Clone, Deserialize)]
+pub struct Transaction {
+    pub timestamp: u64,
+    pub cycles: u64,
+    pub fee: u64,
+    pub kind: TransactionKind,
+}
+
+#[derive(Deserialize, CandidType)]
+pub struct EventsArgs {
+    pub from: Option<u64>,
+    pub limit: u16,
+}
+
+#[derive(CandidType)]
+pub struct EventsConnection<'a> {
+    pub data: &'a [Transaction],
+    pub next_canister_id: Option<Principal>,
+}
+
+pub type TransactionId = u64;
