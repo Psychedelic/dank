21bb78dab658f7906711dc26d62c20931d0298c5
John Kane <john@kanej.me>
2021-08-27 17:38:01 +0100

    test(smoke): add setup instructions on manual load test

--- tests/smoke/lots_of_transactions.sh
+++ tests/smoke/lots_of_transactions.sh
@@ -1,6 +1,12 @@
 # Setup a manual-test-1 and manual-test-2
 # dfx identity new manual-test-1
 # dfx identity new manual-test-2
+#
+# - in root
+# dfx deploy piggy-bank
+# dfx deploy --with-cycles 96000000000000 xtc 
+# ./tests/smoke/lots_of_transactions.sh
+
 
 xtcID=$(dfx canister id xtc)  
 
@@ -13,7 +19,7 @@ dfx identity use manual-test-1
 dfx canister call piggy-bank perform_mint "(record { canister= principal \"$xtcID\"; account=null; cycles=1_000_000_000_000 })"
 dfx canister call piggy-bank perform_mint "(record { canister= principal \"$xtcID\"; account= opt principal \"$manualTest2\"; cycles=1_000_000_000_000 })"
 
-count=20
+count=130
 for index in $(seq $count); do
     if [ `expr $index % 2` -eq 0 ]
     then
